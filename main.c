ac339cd3eb8d7f579decdb6a90ac9328fc25d175
LimSH0731 <lsh00731@gmail.com>
2018-04-26 20:43:49 +0900

    파일 시간 받아오기 함수(수정본)

--- main.c
+++ main.c
@@ -46,6 +46,11 @@ void filestat2(void)
 void filetime1(void)
 {
 	time1=localtime(&stat1.st_mtime);
+	temp.tm_mon=time1->tm_mon;
+	temp.tm_mday=time1->tm_mday;
+	temp.tm_hour=time1->tm_hour;
+	temp.tm_min=time1->tm_min;
+	time1=&temp;
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
