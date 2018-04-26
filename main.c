f31be54a4f00fadab4c0c1cd3303f2e77b77e4e8
GyungTaek Im <ghktks98@gmail.com>
2018-04-26 21:45:12 +0900

    Block Compare Function

--- main.c
+++ main.c
@@ -74,6 +74,13 @@ void sizecmp(void)
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(void)
 {
+	printf("block compare\n");
+	if(stat1.st_blksize>stat2.st_blksize)
+		printf("text1 is bigger.\n");
+	else if(stat1.st_blksize<stat2.st_blksize)
+		printf("text2 is bigger.\n");
+	else
+		printf("sizes are equal.\n");
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
