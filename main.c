448b7bca603249d8c4aaf052cd8145a315029519
GyungTaek Im <ghktks98@gmail.com>
2018-04-26 21:39:15 +0900

    Size Compare Function

--- main.c
+++ main.c
@@ -62,6 +62,13 @@ void filetime2(void)
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(void)
 {
+	printf("size compare\n");
+	if(stat1.st_size<stat2.st_size)
+		printf("text1 is bigger.\n");
+	else if(stat1.st_size>stat2.st_size)
+		printf("text2 is bigger.\n");
+	else
+		printf("sizes are equal.\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
