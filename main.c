5ed9b9878138905c488e4284a258768b69a485b9
LimSH0731 <lsh00731@gmail.com>
2018-04-26 20:26:53 +0900

    파일 시간 받아오기 함수

--- main.c
+++ main.c
@@ -45,11 +45,13 @@ void filestat2(void)
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(void)
 {
+	time1=localtime(&stat1.st_mtime);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(void)
 {
+	time2=localtime(&stat2.st_mtime);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
