76a530936d535538e9db7659e30188f610926c09
Izar111 <34090234+Izar111@users.noreply.github.com>
2018-04-27 04:40:44 +0000

    Update main.c

--- main.c
+++ main.c
@@ -75,9 +75,9 @@ void sizecmp(void)
 void blockcmp(void)
 {
 	printf("block compare\n");
-	if(stat1.st_blksize>stat2.st_blksize)
+	if(stat1.st_blocks>stat2.st_blocks)
 		printf("text1 is bigger.\n");
-	else if(stat1.st_blksize<stat2.st_blksize)
+	else if(stat1.st_blocks<stat2.st_blocks)
 		printf("text2 is bigger.\n");
 	else
 		printf("sizes are equal.\n");
